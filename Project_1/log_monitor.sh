79785caf164006b783a018df93d59e3fcc17b5d6
budomike <89367969+budomike@users.noreply.github.com>
2024-10-18 09:33:42 -0400

    Fixed wording

--- Project_1/log_monitor.sh
+++ Project_1/log_monitor.sh
@@ -2,6 +2,7 @@
 
 # Path to the log file to monitor
 LOGFILE="/var/log/app.log"
+PREVIOUSLOGS="/home/mv/project1/previous_logs.log"
 
 # Database connection details
 DB_USER="mv"
@@ -17,8 +18,8 @@ insert_log_entry() {
     local level=$2
     local message=$3
 
-    # Escape single quotes by replacing each ' with '' (PostgreSQL requires double single quotes for escaping)
-    # s = subsitute g = global/replaces all occurrences 
+    # Escape single quotes by replacing each ' with '' (FATAL message causing problems)
+    # s = substitute g = global/replaces all occurrences 
     local escaped_message=$(echo "$message" | sed "s/'/''/g")
     
     PGPASSWORD=$DB_PASS psql -U $DB_USER -d $DB_NAME -c \
@@ -29,13 +30,13 @@ insert_log_entry() {
 while read line; do
     # Extract log information (timestamp, level, message)
 
-    # awk is command used for text processing, it used here to get parts 1 and 2 for the timestamp
+    # awk is command used for text processing, it used here to get columns 1 and 2 for the timestamp
     TIMESTAMP=$(echo $line | awk '{print $1" "$2}')
 
-    # Get the 3rd part from the line, then tr -d deletes the brackets from the part
+    # Get the 3rd column from the line, then tr -d deletes the brackets from the column
     LEVEL=$(echo $line | awk '{print $3}' | tr -d '[]')
 
-    # cut splits the line based on the delimiter (-d), in this case it is a closing bracket ]
+    # cut splits the line based on the delimiter (-d)
     # then takes everything in the 2nd field (-f2-), which is the log message
     MESSAGE=$(echo $line | cut -d']' -f2-)
     
@@ -47,5 +48,7 @@ while read line; do
     fi
  done < "$LOGFILE"
 
-#Clears the log file to prevent duplicate log entries
+ cat $LOGFILE >> $PREVIOUSLOGS
+
+# Clears the log file to prevent duplicate entries
 > "$LOGFILE"
