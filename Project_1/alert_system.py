dc4f8ef5e5d82b9166e581f0cf6508bd09c3dbf2
budomike <89367969+budomike@users.noreply.github.com>
2024-10-18 09:33:15 -0400

    Fixed wording

--- Project_1/alert_system.py
+++ Project_1/alert_system.py
@@ -1,8 +1,9 @@
-import psycopg
 import time
-    
-time.sleep(2)
+import psycopg
 
+#Delay added to allow table to update first
+time.sleep(2)
+    
 try:
     with psycopg.connect(
         "dbname=project1 user=mv password=jjk123 host=/var/run/postgresql port=5432"
@@ -16,21 +17,24 @@ try:
             FROM log_entries
             """
             cursor.execute(query)
+
+            # record fetches the next row, and returns a tuple e.g. (2, 0)
             record = cursor.fetchone()
             error_count, fatal_count = record
+
             # Handle NoneType by assigning 0 if no errors or fatals exist
             error_count = record[0] if record[0] is not None else 0
             fatal_count = record[1] if record[1] is not None else 0
 
             print(f"Error Count: {error_count}, Fatal Count: {fatal_count}")
 
+            # Implementing timestamp for better logging
             current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
             
             if error_count >= 5 or fatal_count >= 1:
                 print ("Threshold reached at " + current_time + ". Triggering Alert...")
             else:    
-                print("Thresholds not reached. No alert necessary.")
-            cursor.execute
+                print("Thresholds not reached. No alert necessary. Current time is " + current_time + ".")
 
 except Exception as e:
     print("Error connecting to db: ", e)
